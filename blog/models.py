4987822fa37e49cbf5823ccf62d51bf9632bd65d
martoxa <martolivna@gmail.com>
2023-09-08 21:02:34 +0300

    Create a blog post model
    Ref #16

--- blog/models.py
+++ blog/models.py
@@ -1,1 +1,26 @@
 """Models for the blog app."""
+
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    """A class to represent a post."""
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        """Save the post entry."""
+        self.published_date = timezone.now
+        self.save()
+
+    def __str__(self):
+        """Return the post title."""
+        return str(self.title)
